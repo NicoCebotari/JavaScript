c19143450321a4c178d0e3159f7507c3b31bf00d
nicolaecebotari <nicolaecebotari@alumnat.iesdamiahuguet.net>
2024-11-26 12:57:46 +0100

    Hello World inici

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Happy developing ✨')
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").style.color = "red";
+}
+function concatena(){
+    let Nom = document.getElementById("CampNom").value;
+    let Llinatges = document.getElementById("CampLlinatges").value;
+    alert("Hola"  + Nom + " " + Llinatges);
+}
